c96f0e4eb00138c9902e16b5210a755d66be3f76
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-20 22:47:51 +0300

    <FE> Added Search Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import './base.css';
 import './Components/Button/button.css';
 import Navbar from './Components/Navbar/Navbar';
+import Search from './Components/Search/Search';
 
 function App() {
 	return (
 		<div className="App padding-s">
 			<Navbar />
+			<Search />
 		</div>
 	);
 }
---
+++ client/src/Components/Search/Search.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Button from "../Button/Button";
+import '../../base.css'
+import './search.css'
+
+const Search = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchedTerm, setSearchedTerm] = useState('');
+  
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+  
+    const handleSearch = () => {
+        console.log('Perform search with query:', searchQuery);
+        setSearchedTerm(searchQuery);
+        setSearchQuery('');
+      };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            // event.preventDefault();
+            handleSearch();
+        }
+    };
+
+    return (
+      <div className="search-form-container flex flex-col justify-center align-center padding-s gap-s">
+        
+        <div className="search-form flex gap-s">
+            <input
+            className="padding-s"
+            type="text"
+            placeholder="Discover Recipes..."
+            value={searchQuery}
+            onChange={handleInputChange}
+            onKeyUp={handleKeyPress}
+            />
+            <Button onClick={handleSearch}>Search</Button>
+        </div>
+        <div className="search-query flex justify-center align-center">
+            {searchedTerm ? (
+                <p>You searched for: {searchedTerm}</p>
+            ) : (
+                <p>You haven't searched for anything yet</p>
+            )}
+        </div>
+        
+        
+      </div>
+    );
+}
+
+export default Search;

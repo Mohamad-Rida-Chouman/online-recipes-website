153aec622603f7d5e9ba5528bd77dc8d89d2de28
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-22 12:36:50 +0300

    <BE> Fixed Created Tables

--- server/database/migrations/2023_08_22_092013_create_recipe_ingredients_table.php
+++ server/database/migrations/2023_08_22_092013_create_recipe_ingredients_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('recipe_ingredients', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('recipe_id');
+            $table->integer('ingredient_id');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('recipe_ingredients');
     }
 };
